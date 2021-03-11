d1a0a28f05dc5bbc75f0e1a1213e0cda767f9e94
Asyt3 <alexararar@gmail.com>
2021-03-11 16:42:42 +0200

    Last commit

--- src/game/Board.java
+++ src/game/Board.java
@@ -9,6 +9,7 @@ import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
+    //Adds
     private final int B_WIDTH = 300;
     private final int B_HEIGHT = 300;
     private int DOT_SIZE = 10;
@@ -136,7 +137,7 @@ public class Board extends JPanel implements ActionListener {
         tail_up = uTail.getImage();
         ImageIcon dTail = new ImageIcon("src/resources/tail_down.png");
         tail_down = dTail.getImage();
-
+       
 
         ImageIcon sApple = new ImageIcon("src/resources/apple.png");
         apple = sApple.getImage();
